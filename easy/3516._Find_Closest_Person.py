6a05ff70d8c9be67720af6433fca9d8a41fc514b
watanabbee <144165798+watanabbee@users.noreply.github.com>
2025-09-04 07:55:41 -0300

    Create 3516._Find_Closest_Person.py

---
+++ easy/3516._Find_Closest_Person.py
@@ -0,0 +1,27 @@
+#You are given three integers x, y, and z, representing the positions of three people on a number line:
+# x is the position of Person 1.
+# y is the position of Person 2.
+# z is the position of Person 3, who does not move.
+# Both Person 1 and Person 2 move toward Person 3 at the same speed.
+# Determine which person reaches Person 3 first:
+# Return 1 if Person 1 arrives first.
+# Return 2 if Person 2 arrives first.
+# Return 0 if both arrive at the same time.
+# Return the result accordingly.
+# Example 1:
+# Input: x = 2, y = 7, z = 4
+# Output: 1
+# Explanation:
+# Person 1 is at position 2 and can reach Person 3 (at position 4) in 2 steps.
+# Person 2 is at position 7 and can reach Person 3 in 3 steps.
+# Since Person 1 reaches Person 3 first, the output is 1.
+
+class Solution:
+    def findClosest(self, x: int, y: int, z: int) -> int:
+        if abs(x-z)>abs(y-z):
+            return 2
+        elif abs(x-z)<abs(y-z):
+            return 1
+        else:
+            return 0
+        
